0b929bddbc71406c2cf3a95a1af082de2eaf4932
surajkale99 <surajkale99@gmail.com>
2026-03-25 18:16:59 +0530

    stage 1 and 2

---
+++ main.py
@@ -0,0 +1,27 @@
+from src.cnnClassifier.pipeline.stage_01_dataingpipeline import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_preparebasepipeline import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+  logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+  data_ingestion = DataIngestionTrainingPipeline()
+  data_ingestion.main()
+  logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+       logger.exception(e)
+       raise e
+     
+     
+STAGE_NAME = "Prepare base model"
+try:
+  logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+  prepare_base_model = PrepareBaseModelTrainingPipeline()
+  prepare_base_model.main()
+  logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+       logger.exception(e)
+       raise e    
+
